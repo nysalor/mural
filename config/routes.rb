170affaccd41c6d7421a72aba861ca1f49d78d62
Jun Yokoyama <jun@larus.org>
2026-06-15 04:29:24 +0900

    画像をrailsサーバから配信するようにした
    development環境のみ
    production環境ではhttpdが配信する

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "images/:filename", to: "images#show", filename: /.+/
+
   namespace :api do
     get "images/next", to: "images#next"
     get "config",      to: "config#show"
